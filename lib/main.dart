d25daca6a8d4837c870f60debdb38c98d2203f82
azackmatoff <ackmatoff@gmail.com>
2024-07-15 23:07:25 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          useMaterial3: true,
+          // primaryColor: Colors.red,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 19, 214, 6),
+          ),
+          textTheme: const TextTheme(
+            titleMedium: TextStyle(color: Colors.white),
+            titleLarge: TextStyle(color: Colors.white),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(Colors.greenAccent), // Primary button color
+              foregroundColor: WidgetStateProperty.all<Color>(Colors.black),
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+              ),
+            ),
+          ),
+          iconTheme: const IconThemeData(color: Colors.white)),
+      home: const QuizScreen(),
+    );
+  }
+}
---
+++ lib/quiz_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(),
+              Text(
+                'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+                style: Theme.of(context).textTheme.titleLarge,
+                textAlign: TextAlign.center,
+              ),
+              Column(
+                children: [
+                  knopkaniKur(
+                    text: 'TRUE',
+                    style: Theme.of(context).elevatedButtonTheme.style!,
+                  ),
+                  const SizedBox(height: 24),
+                  knopkaniKur(
+                    text: 'FALSE',
+                    style: Theme.of(context).elevatedButtonTheme.style!.copyWith(
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            Colors.red,
+                          ),
+                          foregroundColor: WidgetStateProperty.all<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.check),
+                        Icon(Icons.check),
+                        Icon(Icons.check),
+                        Icon(Icons.close, color: Colors.red),
+                        Icon(Icons.close, color: Colors.red),
+                        Icon(Icons.close, color: Colors.red),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget knopkaniKur({
+    required String text,
+    required ButtonStyle style,
+  }) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: style,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 18.0,
+              ),
+              child: Text(
+                text,
+                style: const TextStyle(fontSize: 22),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
